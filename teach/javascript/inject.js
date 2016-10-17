74a6756f41dd2fdd02dd370793480f3f149ae6a2
Yu-Shan Sun <yushans@g.clemson.edu>
2016-10-17 08:57:44 -0400

    Added the demographic survey page

--- teach/javascript/inject.js
+++ teach/javascript/inject.js
@@ -1,6 +1,6 @@
 function injectTeach(id) {
     $("#" + id).load("teach/html/teachHTML", function() {
-        loadLesson("tutorial/tutorial1.json");
+        loadLesson("tutorial/demographic.json");
         createEditor.session.selection.on("changeCursor", checkLines);
         createEditor.getSession().on("change", checkLines);
     });
