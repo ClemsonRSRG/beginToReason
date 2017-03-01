675e64490a1e7b86aae8823407e23d1792137f31
Yu-Shan Sun <yushans@g.clemson.edu>
2017-03-01 13:36:32 -0500

    More linting

--- left/javascript/inject.js
+++ left/javascript/inject.js
@@ -1,5 +1,4 @@
 /*global loadLesson*/
-/*eslint no-undef: "error"*/
 
 function injectTeach(id) {
     $("#" + id).load("left/html/leftHTML", function() {
--- right/javascript/inject.js
+++ right/javascript/inject.js
@@ -1,5 +1,4 @@
-/*global checkLines*/
-/*eslint no-undef: "error"*/
+/*global Range ace checkLines removeAllVCMarkers*/
 
 var createEditor;
 var author;
@@ -7,34 +6,34 @@ var author;
 function injectCreate(id) {
     $("#" + id).load("right/html/rightHTML", function() {
         injectCreateEditor();
-		
-		// Check to see if we already have an author ID
-		if (localStorage.getItem('author')) {
-			// Reuse the same author ID
-			author = localStorage.getItem('author');
-		}
-		else {
-			// Generate a random number from 1 to 10000
-			author = Math.floor((Math.random() * 10000) + 1);
-			localStorage.setItem('author', author);
-		}
+
+        // Check to see if we already have an author ID
+        if (localStorage.getItem("author")) {
+            // Reuse the same author ID
+            author = localStorage.getItem("author");
+        }
+        else {
+            // Generate a random number from 1 to 10000
+            author = Math.floor(Math.random() * 10000 + 1);
+            localStorage.setItem("author", author);
+        }
     });
 }
 
 function injectCreateEditor() {
-    Range = ace.require('ace/range').Range;
+    var ResolveMode = ace.require("ace/mode/resolve").Mode;
 
+    Range = ace.require('ace/range').Range;
     createEditor = ace.edit("editor");
     createEditor.setTheme("ace/theme/github");
 
     // Set this to RESOLVE mode
-    var ResolveMode = ace.require("ace/mode/resolve").Mode;
     createEditor.session.setMode(new ResolveMode());
 
     // Gets rid of a weird Ace Editor bug
-    //createEditor.$blockScrolling = Infinity;
+    // createEditor.$blockScrolling = Infinity;
 
-    createEditor.getSession().on('change', removeAllVCMarkers);
+    createEditor.getSession().on("change", removeAllVCMarkers);
     createEditor.getSession().selection.on("changeCursor", checkLines);
     createEditor.getSession().on("change", checkLines);
     createEditor.setFontSize(18);
