84d458000fba3264b57b82e882f196c014b78bff
Yu-Shan Sun <yushans@g.clemson.edu>
2017-03-01 12:57:09 -0500

    Forgot to set tutorial0 as the start

--- left/javascript/inject.js
+++ left/javascript/inject.js
@@ -1,6 +1,6 @@
 function injectTeach(id) {
     $("#" + id).load("left/html/leftHTML", function() {
-        loadLesson("problems/tutorial1.json");
+        loadLesson("problems/tutorial0.json");
         createEditor.session.selection.on("changeCursor", checkLines);
         createEditor.getSession().on("change", checkLines);
     });
