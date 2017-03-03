1f91ba31559a7b467619f1ed33f91bce3681af73
Yu-Shan Sun <yushans@g.clemson.edu>
2017-03-03 00:29:35 -0500

    linting

--- right/javascript/vc.js
+++ right/javascript/vc.js
@@ -3,19 +3,18 @@ var verifying = false;
 
 function submitAnswer() {
     /* Protect against multiple requests */
-    if(verifying)
+    if (verifying) {
         return;
-    verifying = true;
+    }
+
+	verifying = true;
     $("#right .footette").attr("class", "footetteDisabled");
-    
+
     var content = createEditor.getValue();
-    
-    if (checkForTrivials(content))
-    {
+
+    if (checkForTrivials(content)) {
         getVCLines(content);
-    } 
-    else
-    {
+    } else {
         $("#dialog-message").html("Sorry, not the intended answer. Try again!");
         $("#dialog-box").dialog("open");
         verifying = false;
@@ -31,20 +30,20 @@ function checkForTrivials(content) {
     if (confirms.length == 0) {
         return false;
     }
-    
+
     var i;
-    for (i = 0 ; i < confirms.length ; i++) {
+    for (i = 0; i < confirms.length; i++) {
         // Remove the "Confirm " so that we can find the variable names
         var statement = confirms[i];
         statement = statement.substr(8);
-        
+
         // Split the string at the conditional
         regex = new RegExp("[<>=]");
         var parts = statement.split(regex);
         if (parts.length != 2) {
             return false;
         }
-        
+
         // Find the variables used on the left side
         var left = parts[0];
         var right = parts[1];
@@ -53,7 +52,7 @@ function checkForTrivials(content) {
         if (variables === null) {
             continue;
         }
-        
+
         // Search for these variables on the right side
         var j;
         for (j = 0 ; j < variables.length ; j++) {
@@ -64,96 +63,103 @@ function checkForTrivials(content) {
             }
         }
     }
-    
+
     return true;
 }
 
 function getVCLines(content) {
     removeAllVCMarkers();
     var socket = new WebSocket("wss://resolve.cs.clemson.edu/teaching/Compiler?job=genVCs&project=Teaching_Project");
-    
-    socket.onmessage = function(message) {
-        if(!verifying) return;
+
+    socket.onmessage = function (message) {
+        if (!verifying) {
+            return;
+        }
         
         // Extract the array of VCs from the message (trust me, this works)
         message = JSON.parse(message.data);
-        if(message.status == "error") {
+        if (message.status == "error") {
             $("#dialog-message").html("Sorry, can't parse your answer. Try again!");
             $("#dialog-box").dialog("open");
             verifying = false;
             $("#right .footetteDisabled").attr("class", "footette");
-            
+
             createEditor.focus();
             return;
         }
         
-        if(message.status != "complete") {
+        if (message.status != "complete") {
             return;
         }
-        
-        if(message.result == "") {
+
+        if (message.result == "") {
 	        $("#dialog-message").html("Sorry, can't parse your answer. Try again!");
 	        $("#dialog-box").dialog("open");
             verifying = false;
             $("#right .footetteDisabled").attr("class", "footette");
-            
+
             createEditor.focus();
             return;
 	    }
-	    
+
         message = decode(message.result);
         message = $(message).text();
         message = JSON.parse(message);
-        
+
         // Simplify the VC information
         VCs = [];
         $.each(message.vcs, function (index, obj) {
             if (typeof obj.vc !== "undefined") VCs.push(obj);
         });
-        
+
         addVCMarkers();
         verifyVCs(createEditor.getValue());
     };
-    
+
     content = encode(content);
     content = toJSON(content);
-    
-    socket.onopen = function() { socket.send(content); };
+
+    socket.onopen = function () {
+        socket.send(content);
+    };
 }
 
 function verifyVCs(content) {
     var socket = new WebSocket("wss://resolve.cs.clemson.edu/teaching/Compiler?job=verify2&project=Teaching_Project");
-    
-    socket.onmessage = function(message) {
+
+    socket.onmessage = function (message) {
         message = JSON.parse(message.data);
-        if(message.status !== "processing") return;
-        
+        if (message.status !== "processing") {
+            return;
+        }
+
         updateMarker(message.result);
     };
-    
+
     content = encode(content);
     content = toJSON(content);
-    
-    socket.onopen = function() { socket.send(content); };
-    socket.onclose = function() {
-        if(succeed) {
+
+    socket.onopen = function () { 
+        socket.send(content); 
+	};
+
+	socket.onclose = function () {
+        if (succeed) {
             approved = true;
             $("#dialog-message").html("Correct. On to the next lesson!");
             $("#dialog-box").dialog("open");
             nextLessonAndSuccess();
-        }
-        else {
+        } else {
             if (currentLesson.self != currentLesson.nextLessonOnFailure) {
                 $("#dialog-message").html("Sorry, not correct. Try this other lesson!");
                 $("#dialog-box").dialog("open");
                 nextLessonAndFailure();
-            }
-            else {
+            } else {
                 $("#dialog-message").html("Sorry, not correct. Try again!");
                 $("#dialog-box").dialog("open");
             }
         }
-        
+
         verifying = false;
         $("#right .footetteDisabled").attr("class", "footette");
         sendData();
