8a3a2b81071fae75636f7743444a446a4335f768
Tim Schwab <tschwab@g.clemson.edu>
2017-03-03 14:18:03 +1100

    Brute force?

--- right/javascript/vc.js
+++ right/javascript/vc.js
@@ -71,10 +71,10 @@ function checkForTrivials(content) {
 function getVCLines(content) {
     removeAllVCMarkers();
     var socket = new WebSocket("wss://resolve.cs.clemson.edu/teaching/Compiler?job=genVCs&project=Teaching_Project");
-
+    
     socket.onmessage = function(message) {
         if(!verifying) return;
-
+        
         // Extract the array of VCs from the message (trust me, this works)
         message = JSON.parse(message.data);
         if(message.status == "error") {
@@ -82,14 +82,15 @@ function getVCLines(content) {
             $("#dialog-box").dialog("open");
             verifying = false;
             $("#right .footetteDisabled").attr("class", "footette");
-
+            
             createEditor.focus();
             return;
         }
+        
         if(message.status != "complete") {
             return;
         }
-
+        
         if(message.result == "") {
 	        $("#dialog-message").html("Sorry, can't parse your answer. Try again!");
 	        $("#dialog-box").dialog("open");
@@ -99,40 +100,40 @@ function getVCLines(content) {
             createEditor.focus();
             return;
 	    }
-
+	    
         message = decode(message.result);
         message = $(message).text();
         message = JSON.parse(message);
-
+        
         // Simplify the VC information
         VCs = [];
         $.each(message.vcs, function (index, obj) {
             if (typeof obj.vc !== "undefined") VCs.push(obj);
         });
-
+        
         addVCMarkers();
         verifyVCs(createEditor.getValue());
     };
-
+    
     content = encode(content);
     content = toJSON(content);
-
+    
     socket.onopen = function() { socket.send(content); };
 }
 
 function verifyVCs(content) {
     var socket = new WebSocket("wss://resolve.cs.clemson.edu/teaching/Compiler?job=verify2&project=Teaching_Project");
-
+    
     socket.onmessage = function(message) {
         message = JSON.parse(message.data);
         if(message.status !== "processing") return;
-
+        
         updateMarker(message.result);
     };
-
+    
     content = encode(content);
     content = toJSON(content);
-
+    
     socket.onopen = function() { socket.send(content); };
     socket.onclose = function() {
         if(succeed) {
@@ -152,7 +153,7 @@ function verifyVCs(content) {
                 $("#dialog-box").dialog("open");
             }
         }
-
+        
         verifying = false;
         $("#right .footetteDisabled").attr("class", "footette");
         sendData();
