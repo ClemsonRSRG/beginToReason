54739733f1f151f9db6cfc7afe3326b75ed79171
Tim Schwab <tschwab@g.clemson.edu>
2017-08-08 14:50:59 -0400

    Set up RESTful routes for the problems

--- main.rb
+++ main.rb
@@ -6,8 +6,16 @@ get "/" do
 	redirect to "/index.html"
 end
 
-get "/problems/:section/:problem" do
+get "/problems/:section/:module/json" do
 	# Eventually, read from a relational database like MySQL or document store
-	# like Redis. But right now, just serve the file.
-	File.read("problems/#{params['section']}/#{params['problem']}")
+	# like Redis, or build them dynamically. But right now, just serve the
+	# file.
+	File.read("problems/#{params['module']}/#{params['problem']}.json")
+end
+
+get "/problems/:section/:module/code" do
+	# Eventually, read from a relational database like MySQL or document store
+	# like Redis, or build them dynamically. But right now, just serve the
+	# file.
+	File.read("problems/#{params['module']}/#{params['problem']}.code")
 end
