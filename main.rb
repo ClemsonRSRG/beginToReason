fa74e396e6f9e7de3e67e1689b7f66b0655a18fb
Tim Schwab <tschwab@g.clemson.edu>
2017-08-08 13:59:54 -0400

    Move problems to their own folder, so that they can be served dynamically eventually

--- main.rb
+++ main.rb
@@ -1,7 +1,13 @@
-require 'sinatra'
+require "sinatra"
 
-set :bind, '0.0.0.0'
+set :bind, "0.0.0.0"
 
-get '/' do
-	'Hello world!'
+get "/" do
+	redirect to "/index.html"
+end
+
+get "/problems/:section/:problem" do
+	# Eventually, read from a relational database like MySQL or document store
+	# like Redis. But right now, just serve the file.
+	File.read("problems/#{params['section']}/#{params['problem']}")
 end
