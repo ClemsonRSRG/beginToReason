7ba0d66c1c1ed6f3f6e55054a744f870c47ebf79
Tim Schwab <tschwab@g.clemson.edu>
2017-08-08 14:55:14 -0400

    Whoops, quick fix

--- main.rb
+++ main.rb
@@ -6,14 +6,14 @@ get "/" do
 	redirect to "/index.html"
 end
 
-get "/problems/:section/:module/json" do
+get "/problems/:module/:problem/json" do
 	# Eventually, read from a relational database like MySQL or document store
 	# like Redis, or build them dynamically. But right now, just serve the
 	# file.
 	File.read("problems/#{params['module']}/#{params['problem']}.json")
 end
 
-get "/problems/:section/:module/code" do
+get "/problems/:module/:problem/code" do
 	# Eventually, read from a relational database like MySQL or document store
 	# like Redis, or build them dynamically. But right now, just serve the
 	# file.
