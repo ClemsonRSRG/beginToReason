746e9667a230ebac4529a525e965d70890ce582b
Tim Schwab <tschwab@g.clemson.edu>
2017-09-07 14:08:40 -0400

    Added csv option to export

--- export.sh
+++ export.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
-echo "Username: "
+
+echo -n "Username: "
 read user
 echo -n "Password: "
-echo
 read -s pass
+echo
 
-mongoexport -u ${user} -p ${pass} --authenticationDatabase resolve --db resolve --collection data --type=json --out data.json
+echo -n "Output format: (json/csv) "
+read format
+
+if [ "$format" == "json" ]
+then
+	mongoexport -u ${user} -p ${pass} --authenticationDatabase resolve --db resolve --collection data --type=json --out data.json
+elif [ "$format" == "csv" ]
+then
+	fields="type,module,name,author,code,time,correct"
+	mongoexport -u ${user} -p ${pass} --authenticationDatabase resolve --db resolve --collection data --type=csv --out data.csv --fields ${fields}
+else
+	echo "Format not supported."
+fi
+
 
