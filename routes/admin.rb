3b9ddd4eb98dd1a1c6e7df0567f1a2dfd0f46709
Yu-Shan Sun <yushans@g.clemson.edu>
2017-10-19 10:58:08 -0400

    Display basic statistics from the db

--- routes/admin.rb
+++ routes/admin.rb
@@ -1,12 +1,17 @@
 # Handles everything to do with the admin page.
 
 require 'sinatra'
-
 # The different routes to the different admin subpages.
 get '/admin' do
 	redirect to '/admin/overview'
 end
 
 get '/admin/overview' do
+	# Compute the different data to be displayed in the overview
+	loggedData = settings.mongo[:data]
+	@totalUsers = loggedData.distinct( "author" ).length
+	@totalEntries = loggedData.count()
+	@totalCorrectEntries = loggedData.count( { correct: true } )
+
 	erb :"admin/overview" , :layout => :"admin/layout"
 end
